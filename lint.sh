fa9019f4d986338c3449afc222ae5d6b6bd0e6c3
Bertrand Roussel <broussel@sierrawireless.com>
2020-05-06 05:49:24 -0700

    Add option to run directly on host

--- lint.sh
+++ lint.sh
@@ -7,11 +7,21 @@ set -o nounset
 export PATH=$PATH:/usr/local/bin
 
 CMD="hadolint"
+SCRIPT_DIR="$( cd `dirname "$0"` && pwd )"
+export PATH="$SCRIPT_DIR:$PATH"
 
 if ! command -v hadolint >/dev/null; then
     if [ -n "$(command -v brew)" ]; then
         >&2 echo "hadolint: installing using brew"
         brew install hadolint
+    elif [ -n "$(command -v curl)" ]; then
+        VERSION="v1.17.6"
+        URL="https://github.com/hadolint/hadolint/releases/download/$VERSION/hadolint-$(uname)-x86_64"
+        if ! curl -fsSL "$URL" -o "$SCRIPT_DIR/hadolint"; then
+            >&2 echo "hadolint: unable to download from $URL"
+            exit 1
+        fi
+        chmod +x "$SCRIPT_DIR/hadolint"
     elif [ -n "$(command -v docker)" ]; then
         >&2 echo "hadolint: running with docker"
         CMD="docker run --rm --volume=$PWD:$PWD:ro --workdir=$PWD hadolint/hadolint hadolint"
