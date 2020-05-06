c973d3abebe8e647ac0c00c97e03017520269eb8
Bertrand Roussel <broussel@sierrawireless.com>
2020-05-06 05:33:25 -0700

    Add option to run with Docker

--- lint.sh
+++ lint.sh
@@ -6,12 +6,19 @@ set -o nounset
 
 export PATH=$PATH:/usr/local/bin
 
-if ! which hadolint &>/dev/null; then
-    >&2 echo 'hadolint command not found'
-    if [ -z "$(command -v brew)" ]; then
-        exit 1
+CMD="hadolint"
+
+if ! command -v hadolint >/dev/null; then
+    if [ -n "$(command -v brew)" ]; then
+        >&2 echo "hadolint: installing using brew"
+        brew install hadolint
+    elif [ -n "$(command -v docker)" ]; then
+        >&2 echo "hadolint: running with docker"
+        CMD="docker run --rm --volume=$PWD:$PWD:ro --workdir=$PWD hadolint/hadolint hadolint"
+    else
+        >&2 echo "hadolint: command not found and don't know how to install"
     fi
-    brew install hadolint
 fi
 
-hadolint "$@"
+set -x
+$CMD "$@"
